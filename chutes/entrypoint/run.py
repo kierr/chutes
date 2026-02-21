aa48b0948a37f8a491087934fb3262595b7464bf
Jon Durbin <jon@jondurbin.com>
2026-02-21 01:32:39 +0000

    f

--- chutes/entrypoint/run.py
+++ chutes/entrypoint/run.py
@@ -1888,7 +1888,6 @@ def run_chute(
             aegis_commitment = init_aegis("42")
             aegis_nonce = aegis_get_nonce()
 
-            os.environ["LD_PRELOAD"] = "/usr/local/lib/chutes-aegis.so"
             inspecto_hash = await generate_hash(hash_type="base")
             print(inspecto_hash)
             return
